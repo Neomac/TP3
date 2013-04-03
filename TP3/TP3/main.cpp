0c903eda72ab8086e5814af4fd81ee685a654025
Neomac <antoine18@gmail.com>
2013-04-03 18:47:38 +0200

    Initial Commit

---
+++ TP3/TP3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TP3
+//
+//  Created by Antoine Galluet on 03/04/13.
+//  Copyright (c) 2013 Antoine Galluet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
